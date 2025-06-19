a2f85723c2e0bf1e54cf5d6a7453fb87d3dd9f2b
Christel EHRHART <contact@ce-formation.com>
2025-06-19 14:30:36 +0200

    ajout du lien vers edit_article.php (à créer)

--- _partial/article.php
+++ _partial/article.php
@@ -4,7 +4,17 @@
 				<h3 class="mb-0"><?php echo $objArticle->getTitle();/*$arrDetArticle['article_title'];*/ ?></h3>
 				<div class="mb-1 text-body-secondary"><?php echo $objArticle->getDateFormat(); ?> (<?php echo $objArticle->getAuthor(); ?>)</div>
 				<p class="mb-auto"><?php echo $objArticle->getSummary(); ?> </p>
-				<a href="#" class="icon-link gap-1 icon-link-hover stretched-link">Lire la suite</a>
+				<a href="#" class="icon-link gap-1 icon-link-hover">Lire la suite</a>
+				
+				<?php
+					if (isset($_SESSION['id']) && $_SESSION['id'] != '') {
+				?>
+				<p><a alt="Modifier un article" 
+						href="edit_article.php?id=<?php echo $objArticle->getId(); ?>">Modifier l'article</a></p>
+				<?php
+					}
+				?>				
+				
 			</div>
 			<div class="col-auto d-none d-lg-block">
 				<img class="bd-placeholder-img" width="200" height="250" alt="<?php echo $objArticle->getTitle();; ?>" src="assets/images/<?php echo $objArticle->getImg(); ?>">
