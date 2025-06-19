16f52eddfacaa1664df45bbe6cd9e128e4644cac
Christel EHRHART <contact@ce-formation.com>
2025-06-19 11:28:41 +0200

    modification objet de : edit_account

--- edit_account.php
+++ edit_account.php
@@ -17,55 +17,39 @@
 	// Récupérer les informations de l'utilisateur connecté
 	$intID	= $_SESSION['id'];
 	
-	// Récupère les utilisateurs en fonction de son identifiant
-	require("connexion.php");
-	$strQuery	= "SELECT user_name, user_firstname, user_mail
-					FROM users 
-					WHERE user_id = :id;";
-	$strRqPrep	= $db->prepare($strQuery);	
-	$strRqPrep->bindValue(":id", $intID, PDO::PARAM_INT);
-	$strRqPrep->execute();
-	$arrUser	= $strRqPrep->fetch();
+	// Récupère l'utilisateur en fonction de son identifiant
+	require("models/user_model.php");
+	$objUserModel 	= new UserModel();
+	$arrUser		= $objUserModel->findById($intID);
+	
+	require("entities/user_entity.php");
+	$objUser = new User();
 	
 	// Définition des variable, du formulaire ou de la bdd
-	$strName		= $_POST['name']??$arrUser['user_name'];
-	$strFirstname	= $_POST['firstname']??$arrUser['user_firstname'];
-	$strMail		= $_POST['mail']??$arrUser['user_mail'];
-	$strPwd			= $_POST['pwd']??"";
 	$strConfirm_pwd	= $_POST['confirm_pwd']??"";
-	
-	// Enlever les espaces avant et après => trim()
-	$strName		= trim($strName);
-	$strFirstname	= trim($strFirstname);
-	$strMail		= strtolower(trim($strMail));
 	
 	// initialise le tableau des erreurs
 	$arrErrors	= array(); 
 	// Si le formulaire a été envoyé
 	if (count($_POST) > 0){
+		$objUser->hydrate($_POST);
+
 		// Si l'utilisateur n'a pas saisi son nom
-		if ($strName == ""){
+		if ($objUser->getName() == ""){
 			$arrErrors['name'] = "Le nom est obligatoire";
 		}
 		// Si l'utilisateur n'a pas saisi son prénom
-		if ($strFirstname == ""){
+		if ($objUser->getFirstname() == ""){
 			$arrErrors['firstname'] = "Le prénom est obligatoire";
 		}
 		// Si l'utilisateur n'a pas saisi son mail
-		if ($strMail == ""){
+		if ($objUser->getMail() == ""){
 			$arrErrors['mail'] = "Le mail est obligatoire";
-		}elseif(!filter_var($strMail, FILTER_VALIDATE_EMAIL)){
+		}elseif(!filter_var($objUser->getMail(), FILTER_VALIDATE_EMAIL)){
 			$arrErrors['mail'] = "Le mail n'est pas valide";
-		}elseif($strMail != $arrUser['user_mail']){
+		}elseif($objUser->getMail() != $arrUser['user_mail']){
 			// Si l'adresse mail a été changée, vérifier son unicité en bdd
-			require("connexion.php");
-			$strQuery	= "SELECT user_mail
-							FROM users 
-							WHERE user_mail = :mail;";
-			$strRqPrep	= $db->prepare($strQuery);	
-			$strRqPrep->bindValue(":mail", $strMail, PDO::PARAM_STR);
-			$strRqPrep->execute();
-			$arrUserExist	= $strRqPrep->fetch();
+			$arrUserExist	= $objUserModel->findUserByMail($objUser->getMail());
 			// Si j'ai un résultat => erreur
 			if($arrUserExist !== false){
 				$arrErrors['mail'] = "Le mail est déjà utilisé";
@@ -73,41 +57,24 @@
 		}
 		
 		// Si l'utilisateur veut modifier son mot de passe
-		if ( ($strPwd != "") && ($strPwd != $strConfirm_pwd) ){
+		if ( ($objUser->getPwd() != "") 
+			&& ($objUser->getPwd() != $strConfirm_pwd) ){
 			$arrErrors['confirm_pwd'] = "Le mot de passe et sa confirmation ne correspondent pas";
 		}
 		
 		// Si le formulaires est OK
 		if (count($arrErrors) == 0){
 			// Modifier les infos en BDD
-			$strQuery		= "UPDATE users 
-								SET user_name = :name,
-									user_firstname = :firstname,
-									user_mail = :mail";
-			if ($strPwd != ""){
-				$strQuery	.=	" , user_pwd = :pwd ";
-			}
-			$strQuery	.=	" WHERE user_id = :id; ";
-			$strRqPrep	= $db->prepare($strQuery);	
-			$strRqPrep->bindValue(":name", $strName, PDO::PARAM_STR);
-			$strRqPrep->bindValue(":firstname", $strFirstname, PDO::PARAM_STR);
-			$strRqPrep->bindValue(":mail", $strMail, PDO::PARAM_STR);
-			$strRqPrep->bindValue(":id", $intID, PDO::PARAM_INT);
+			$objUserModel->update($objUser);
 			
-			if ($strPwd != ""){
-				// Hacher le mot de passe
-				$strPwdHash = password_hash($strPwd, PASSWORD_DEFAULT);
-				$strRqPrep->bindValue(":pwd", $strPwdHash, PDO::PARAM_STR);
-			}
-			
-			$strRqPrep->execute();
-			
-			$_SESSION['prenom'] = $strFirstname;
-			$_SESSION['nom'] 	= $strName;
+			$_SESSION['prenom'] = $objUser->getFirstname();
+			$_SESSION['nom'] 	= $objUser->getName();
 			$_SESSION['message']= "Vos informations ont bien été modifiées";
 			// Redirection vers la page d'accueil
 			header("Location:index.php");
 		}		
+	}else{
+		$objUser->hydrate($arrUser);
 	}
 ?>
 <?php
@@ -125,17 +92,17 @@
 		<legend>Informations</legend>
 		<p>
 			<label for="name">Nom</label>
-			<input name="name" value="<?php echo $strName; ?>" id="name" class="form-control 
+			<input name="name" value="<?php echo $objUser->getName(); ?>" id="name" class="form-control 
 				<?php if(isset($arrErrors['name'])){ echo 'is-invalid'; } ?>" type="text" >
 		</p>
 		<p>
 			<label for="firstname">Prénom</label>
-			<input name="firstname" value="<?php echo $strFirstname; ?>" id="firstname" class="form-control 
+			<input name="firstname" value="<?php echo $objUser->getFirstname(); ?>" id="firstname" class="form-control 
 				<?php if(isset($arrErrors['firstname'])){ echo 'is-invalid'; } ?>" type="text" >
 		</p>
 		<p>
 			<label for="mail">Mail</label>
-			<input name="mail" value="<?php echo $strMail; ?>" id="mail" class="form-control 
+			<input name="mail" value="<?php echo $objUser->getMail(); ?>" id="mail" class="form-control 
 				<?php if(isset($arrErrors['mail'])){ echo 'is-invalid'; } ?>" type="text" >
 		</p>
 	</fieldset>
--- models/user_model.php
+++ models/user_model.php
@@ -26,12 +26,49 @@ class UserModel extends Connexion{
 		return $arrUser;
 	}
 	
+	public function findById(int $intId):array {
+		$strQuery	= "SELECT user_name, user_firstname, user_mail
+					FROM users 
+					WHERE user_id = :id;";
+		$strRqPrep	= $this->_db->prepare($strQuery);	
+		$strRqPrep->bindValue(":id", $intId, PDO::PARAM_INT);
+		$strRqPrep->execute();
+		$arrUser	= $strRqPrep->fetch();
+		
+		return $arrUser;
+	}
+	
 	public function insert(object $objUser){
 		$strQuery		= "INSERT INTO users 
 			(user_name, user_firstname, user_mail, user_pwd)
 			VALUES 
 			('".$objUser->getName()."', '".$objUser->getFirstname()."', '".$objUser->getMail()."', '".$objUser->getPwdHash()."');";
 		$this->_db->exec($strQuery);
+	}
+	
+	public function update(object $objUser){
+			$strQuery		= "UPDATE users 
+								SET user_name = :name,
+									user_firstname = :firstname,
+									user_mail = :mail";
+			if ($objUser->getPwd() != ""){
+				$strQuery	.=	" , user_pwd = :pwd ";
+			}
+			$strQuery	.=	" WHERE user_id = :id; ";
+			$strRqPrep	= $this->_db->prepare($strQuery);	
+			$strRqPrep->bindValue(":name", $objUser->getName(), PDO::PARAM_STR);
+			$strRqPrep->bindValue(":firstname", $objUser->getFirstname(), PDO::PARAM_STR);
+			$strRqPrep->bindValue(":mail", $objUser->getMail(), PDO::PARAM_STR);
+			$strRqPrep->bindValue(":id", $_SESSION['id'], PDO::PARAM_INT);
+			
+			if ($objUser->getPwd() != ""){
+				// Hacher le mot de passe
+				//$strPwdHash = password_hash($strPwd, PASSWORD_DEFAULT);
+				$strRqPrep->bindValue(":pwd", $objUser->getPwdHash(), PDO::PARAM_STR);
+			}
+			
+			$strRqPrep->execute();		
+		
 	}
 	
 }
